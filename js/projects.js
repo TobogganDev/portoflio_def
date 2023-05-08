588e8eee519ba09593f6c0cbe23c6f891605f4b7
Thomas Doret-Gaïsset <doretg.t@gmail.com>
2023-05-08 22:13:18 +0200

    :sparkles: slider for project

---
+++ js/projects.js
@@ -0,0 +1,37 @@
+let activeIndex = 0;
+
+const slides = document.querySelectorAll('article');
+
+const handleLeftClick = () => {
+  console.log('left click');
+  const nextIndex = activeIndex - 1 >= 0 ? activeIndex - 1 : slides.length - 1;
+  
+  const currentSlide = document.querySelector(`[data-index="${activeIndex}"]`),
+  nextSlide = document.querySelector(`[data-index="${nextIndex}"]`);
+  
+  currentSlide.dataset.status = 'after';
+  
+  nextSlide.dataset.status = "becoming-active-from-before";
+  
+  setTimeout(() => {
+    nextSlide.dataset.status = "active";
+    activeIndex = nextIndex;
+  });
+};
+
+const handleRightClick = () => {
+  console.log('right click');
+  const nextIndex = activeIndex + 1 <= slides.length - 1 ? activeIndex + 1 : 0;
+  
+  const currentSlide = document.querySelector(`[data-index="${activeIndex}"]`),
+  nextSlide = document.querySelector(`[data-index="${nextIndex}"]`);
+  
+  currentSlide.dataset.status = "before";
+  
+  nextSlide.dataset.status = "becoming-active-from-after";
+  
+  setTimeout(() => {
+    nextSlide.dataset.status = "active";
+    activeIndex = nextIndex;
+  });
+};
